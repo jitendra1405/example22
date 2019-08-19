cdf577182814cc542db316cd906e0e5fc004e489
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-19 12:28:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 $dbconn = pg_connect("host=ec2-50-19-124-157.compute-1.amazonaws.com port=5432 dbname=d58o173hbaukt7 user=nfltllotkrgbnc password=6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0");
 
-$sql = "INSERT into contact.contact (FirstName) values ('Jagish Chand')" ;
+$sql = "INSERT into contact.contact (FirstName) values ('AAAAAAAAAAAAAAAAFFrrooo')" ;
                 pg_query($dbconn, $sql); 
+$sql = "select name from contact.contact";
+
+                            $resultset = pg_query($dbconn, $sql);
+                            while($row = pg_fetch_array($resultset)) {
+                                echo '<tr>
+                                        <td>'.$row[0].'</td>
+                                        
+                                    </tr>'; 
+                            }
 
                             pg_close($dbconn);
 ?>
