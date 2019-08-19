d3104d510f662e2757a04b1d565e7cb61ed04e62
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-19 12:07:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,47 @@
 
 <?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
 
-   $con = "dbname=d58o173hbaukt7 host=ec2-50-19-124-157.compute-1.amazonaws.com port=5432 user=nfltllotkrgbnc password=6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0 sslmode=require";
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
 
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
 
-   if (!$con) 
-   {
-     echo "Database connection failed.";
-   }
-   else 
-   {
-     echo "Database connection success.";
-     echo $con;	   
-	$sql = "select FirstName from contact.contact";
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
 
-                            $resultset = pg_query($con, $sql);
-                            echo $resultset;
-   }
-    
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+
+$host = "ec2-174-129-227-80.compute-1.amazonaws.com";
+$user = "hqolsipojtnecl";
+$password = "64306f4dd68d73a8a477652e710ede2520086af42fc9e2dca049f2eb1debfab6";
+$dbname = "de4bcai2dahkqm";
+$port = "5432";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("select FirstName from contact.contact"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
 ?>
-	
