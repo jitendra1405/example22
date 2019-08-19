645b1e26d11b041ae52b33b850e2c549cbc55823
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-19 14:43:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 echo
 "<form action='' method='post'>
 <input type='submit' name='use_button' value='something' />
+Name: <input type='text' name='name'/>
 </form>";
 
 if(isset($_POST['use_button']))
