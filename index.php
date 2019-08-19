ff6e0e901ce0012d31773a0fbb9c2cc5cf4b1a2a
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-19 14:02:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <form method="POST">
 <input type=SUBMIT action="<?php
 $dbconn = pg_connect("host=ec2-50-19-124-157.compute-1.amazonaws.com port=5432 dbname=d58o173hbaukt7 user=nfltllotkrgbnc password=6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0");
-function select(){
+
                echo "The select function is called.";
-            }
+            
 ?>" 
 value="My Button">
 </form>
