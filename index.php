9acc42c5682c966ba887f3f884d52d9d40ecc14f
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-19 14:57:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$dbconn = pg_connect("host=ec2-50-19-124-157.compute-1.amazonaws.com port=5432 dbname=d58o173hbaukt7 user=nfltllotkrgbnc password=6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0");
 
 echo
 "<form action='' method='post'>
@@ -9,7 +10,8 @@ Name: <input type='text' name='name'/>
 if(isset($_POST['use_button']))
 {
 	$x=$_POST['name'];
-	echo $x;
+	$sql = "INSERT into contact.contact (FirstName) values ('".$x."')" ;
+                pg_query($dbconn, $sql); 
 }
-
+pg_close($dbconn);
 ?>
