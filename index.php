f0daa66ec1ec324f3f47f4c0cdcdd16325ebcac4
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-19 13:27:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,12 @@
 <html lang="en">
   <body>
     Name: <input type="text" name="name">
-    <input type="submit" name="submit" value="Submit" onclick="abc()">
+    <input type="submit" name="submit" value="Submit" onclick="select()">
 <?php
 $dbconn = pg_connect("host=ec2-50-19-124-157.compute-1.amazonaws.com port=5432 dbname=d58o173hbaukt7 user=nfltllotkrgbnc password=6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0");
-function abc(){
-$sql = "INSERT into contact.contact (FirstName) values ('".$name."')" ;
-                pg_query($dbconn, $sql); 
-
-
-                            pg_close($dbconn);
-}
+function select(){
+               echo "The select function is called.";
+            }
 ?>
 </body>
 </html>
